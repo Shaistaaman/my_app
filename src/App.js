c6ff538d6f573599176cffb37d0292ae0589e9b3
Shaistaaman <57103671+Shaistaaman@users.noreply.github.com>
2020-06-10 23:53:24 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Room from './Room.js';
+
+function App() {
+  return (
+    <div> 
+      <Room />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Room.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import "./Room.css";
+function Room() {
+    let [isLit, setLit] = useState(true);
+    let [temp, setTemp] = useState(34);
+      
+  return (
+    <div className={`room ${isLit? "lit" : "dark"}`}>      
+        <h2>This Room is = {isLit ? "Lit" : "Dark"} </h2>
+      <br />
+      <button onClick={() => setLit(isLit = true)}>On</button>
+      <br />   
+      <button onClick={() => setLit(isLit = false)}>Off</button>
+      <br /> 
+        <h2>Room Temperature  = {temp}  </h2>
+     
+      <br />     
+      <button onClick={() => setTemp(++temp)}>
+            Increase Temperature
+       </button>
+       <br />
+       <button onClick={() => setTemp(--temp)}>
+            Decrease Temperature
+       </button>
+    </div>
+  );
+}
+
+export default Room;
+
